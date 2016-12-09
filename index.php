27464898d3f9b51fa9b3baf6178a852986c19f6d
awhited <auw6@pitt.edu>
2016-12-08 23:53:16 -0500

    removed some junk

--- index.php
+++ index.php
@@ -4,11 +4,6 @@
     <title>Project</title>
     <meta charset="UTF-8"> 
     <link rel="stylesheet" type="text/css" href="css/css.css">
-    <!--<link rel="stylesheet" 
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
-          crossorigin="anonymous"
-    >-->
   </head>
   <body>
     <ul class="topnav" id="topNav"></ul>
