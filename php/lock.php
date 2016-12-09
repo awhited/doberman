3b15f8ff18ba4cc3e816f0707f5004d72e16387c
awhited <auw6@pitt.edu>
2016-12-08 22:18:12 -0500

    updated lock.php

--- php/lock.php
+++ php/lock.php
@@ -1,4 +1,4 @@
-<? php
+<?php
 
 // This file is here to get around JavaScripts bulldozer tendancy of running every goddamn function in the background and trying to send them all out as fast as possible. Take a chill pill JS, wait on this. 
 
